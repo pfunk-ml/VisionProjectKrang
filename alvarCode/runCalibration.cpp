6ffcb870b246cb4de312a55b6af3c6850175bb7e
ana-GT <ahuaman3@gatech.edu>
2014-04-11 12:20:08 -0400

    Added runCalibration.cpp

--- alvarCode/runCalibration.cpp
+++ alvarCode/runCalibration.cpp
@@ -1,110 +1,115 @@
+/**
+ * @file runCalibration.cpp
+ * @brief
+ */
+#include <Camera.h>
+
 #include <iostream>
-#include "Camera.h"
 #include "CvTestbed.h"
 #include "Shared.h"
-using namespace alvar;
-using namespace std;
 
-const int calib_count_max=50;
-const int etalon_rows=6;
-const int etalon_columns=8;
-std::stringstream calibrationFilename;
 
-void videocallback(IplImage *image)
-{
+
+// Number of captures needed to perform the calibration
+const int gCalib_count_max=50;
+
+// Size of marker's edge in cm.
+const int gEtalon_square_size = 2.5;
+
+// Number of internal corners in the pattern
+const int gEtalon_rows=6;
+const int gEtalon_columns=8;
+
+// Name of calibration file to store the results
+std::stringstream gCalibrationFilename;
+
+
+/**
+ * @brief Function to be called in the loop
+ */
+void videocallback( IplImage *_img ) {
+    
     static bool calibrated = false;
     static int calib_count=0;
-    static Camera cam;
-    static ProjPoints pp;
+    static alvar::Camera cam;
+    static alvar::ProjPoints pp;
     static int64 prev_tick=0;
     static bool initialized = false;
 
     if (!initialized) {
-      cam.SetRes(image->width, image->height);
-      prev_tick = cvGetTickCount();
-      initialized = true;
+	cam.SetRes( _img->width, 
+		    _img->height );
+	prev_tick = cvGetTickCount();
+	initialized = true;
     }
   
-    bool flip_image = (image->origin?true:false);
+    bool flip_image = ( _img->origin? true:false );
     if (flip_image) {
-        cvFlip(image);
-        image->origin = !image->origin;
+        cvFlip(_img);
+        _img->origin = !_img->origin;
     }
 
-    assert(image);
+    assert(_img);
+    
     if (!calibrated) {
         // If we have already collected enough data to make the calibration
         // - We are ready to end the capture loop
         // - Calibrate
         // - Save the calibration file
-        if (calib_count >= calib_count_max) {
-            std::cout<<"Calibrating..."<<endl;
+        if ( calib_count >= calib_count_max ) {
+            std::cout<<"*** Start calibrating process, please wait ***"<<endl;
             calib_count = 0;
             cam.Calibrate(pp);
-            pp.Reset();
-            cam.SaveCalib(calibrationFilename.str().c_str());
-            std::cout<<"Saving calibration: "<<calibrationFilename.str()<<endl;
+            pp.Reset(); 
+            cam.SaveCalib( gCalibrationFilename.str().c_str() );
+            std::cout<<"*** Saving calibration: "<< gCalibrationFilename.str() << std::endl;
             calibrated = true;
         } 
         // If we are still collecting calibration data
         // - For every 1.5s add calibration data from detected 7*9 chessboard (and visualize it if true)
         else {
             int64 tick = cvGetTickCount();
-            if ((tick - prev_tick) > (cvGetTickFrequency() * 1000 * 1000 * 1.5)) {
-                if (pp.AddPointsUsingChessboard(image, 2.8, etalon_rows, etalon_columns, true)) {
+            if( (tick - prev_tick) > (cvGetTickFrequency() * 1000 * 1000 * 1.5) ) {
+                if( pp.AddPointsUsingChessboard( _img, gEtalon_square_size, 
+						 gEtalon_rows, gEtalon_columns, true)) {
                     prev_tick = tick;
                     calib_count++;
-                    cout<<calib_count<<"/"<<calib_count_max<<endl;
+		    std::cout << "Calibration captures: "<< calib_count << "/" << calib_count_max << std::endl;
                 }
             }
         }
+	
+	// If already calibrated - NOT SURE WHAT THIS DOES
     } else {
-        if (pp.AddPointsUsingChessboard(image, 2.5, etalon_rows, etalon_columns, true)) {
-            Pose pose;
-            cam.CalcExteriorOrientation(pp.object_points, pp.image_points, &pose);
-            cam.ProjectPoints(pp.object_points, &pose, pp.image_points);
-            for (size_t i=0; i<pp.image_points.size(); i++) {
-                cvCircle(image, cvPoint((int)pp.image_points[i].x, (int)pp.image_points[i].y), 6, CV_RGB(0, 0, 255));
+        if ( pp.AddPointsUsingChessboard( _img, gEtalon_square_size, 
+					  gEtalon_rows, gEtalon_columns, true) ) {
+	    alvar::Pose pose;
+            cam.CalcExteriorOrientation( pp.object_points, pp.image_points, &pose );
+            cam.ProjectPoints( pp.object_points, &pose, pp.image_points );
+            for (size_t i=0; i < pp.image_points.size(); i++ ) {
+                cvCircle( _img, cvPoint( (int)pp.image_points[i].x, 
+					 (int)pp.image_points[i].y), 
+			  6, CV_RGB(0, 0, 255) );
             }
             pp.Reset();
         }
     }
-
+    
     if (flip_image) {
-        cvFlip(image);
-        image->origin = !image->origin;
+        cvFlip(_img);
+        _img->origin = !_img->origin;
     }
 }
 
-int main(int argc, char *argv[])
+/**
+ * @function main
+ */
+int main( int argc, char *argv[] )
 {
     try {
-        // Output usage message
-        std::string filename(argv[0]);
-        filename = filename.substr(filename.find_last_of('\\') + 1);
-        std::cout << "SampleCamCalib" << std::endl;
-        std::cout << "==============" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Description:" << std::endl;
-        std::cout << "  This is an example of how to use the 'Camera' and 'ProjPoints' classes" << std::endl;
-        std::cout << "  to perform camera calibration. Point the camera to the chessboard" << std::endl;
-        std::cout << "  calibration pattern (see ALVAR.pdf) from several directions until 50" << std::endl;
-        std::cout << "  calibration images are collected. A 'calib.xml' file that contains the" << std::endl;
-        std::cout << "  internal parameters of the camera is generated and can be used by other" << std::endl;
-        std::cout << "  applications that require a calibrated camera." << std::endl;
-        std::cout << std::endl;
-        std::cout << "Usage:" << std::endl;
-        std::cout << "  " << filename << " [device]" << std::endl;
-        std::cout << std::endl;
-        std::cout << "    device    integer selecting device from enumeration list (default 0)" << std::endl;
-        std::cout << "              highgui capture devices are prefered" << std::endl;
-        std::cout << std::endl;
-        std::cout << "Keyboard Shortcuts:" << std::endl;
-        std::cout << "  q: quit" << std::endl;
-        std::cout << std::endl;
 
         // Initialise CvTestbed
-        CvTestbed::Instance().SetVideoCallback(videocallback);
+        CvTestbed::Instance().SetVideoCallback( videocallback );
 
         // Enumerate possible capture plugins
         CaptureFactory::CapturePluginVector plugins = CaptureFactory::instance()->enumeratePlugins();

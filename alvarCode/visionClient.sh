6a97c98386da4227bd6065247064c1d22fb34873
Ana Huaman <ahuaman3@gatech.edu>
2014-04-16 16:15:09 -0400

    Added script

--- alvarCode/visionClient.sh
+++ alvarCode/visionClient.sh
@@ -1,12 +1,12 @@
-
 #!/bin/bash
 
 # Put this script on the same
 # directory where the executables are
+BIN_DIR="/home/ana/Research/VisionProjectKrang/alvarCode/bin"
 
 # Cleanup any channel that might be wandering around
 echo "-- Start cleanup process! --"
-"./cleanup"
+"${BIN_DIR}/cleanup"
 
 echo "-- Cleaned channels! --"
 
@@ -20,27 +20,37 @@ ach -C krang_vision -o 666  -m 10 -n 64
 # Camera 1 is on /dev/video1
 
 # For some reason we have to start camera 1 first, so keep the order
-echo "[START] Firing up camera 1"
-"./camProcess" 1 1 &
+echo "************************************"
+echo "[START] Firing up camera 1 [START]"
+echo "************************************"
+"${BIN_DIR}/camProcess" 1 1 &
 sleep 3s
-echo "[DONE] Finishing starting camera 1"
 
+
+echo "\n[DONE] Finishing starting camera 1"
+
+echo "************************************"
 echo "[START] Firing up camera 0"
-"./camProcess" 0 0 &
+echo "************************************"
+"${BIN_DIR}/camProcess" 0 0 &
 sleep 3s
 echo "[DONE] Finishing starting camera 0"
 
 
 # Sleep for a bit to give the cameras time to run a bit
+echo "************************************"
 echo "[SLEEP] Sleep for 2 seconds before starting the central main--"
 sleep 2s
 echo "[START] Start the main camera process!--"
+echo "************************************"
 
 # Run the camCentralProcessMain. This retrieve the info from the cameras 
-"./camCentralProcessMain" &
+"${BIN_DIR}/camCentralProcessMain" &
 
 # Sleep just for the heck of it
 sleep 2s
 
 # Done
+echo "************************************"
 echo "Done setting ach channels for cameras and krang_vision"
+echo "************************************"

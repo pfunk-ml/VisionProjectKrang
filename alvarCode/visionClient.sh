ae154997339e164884b80a347cf415872470fc6b
Ana Huaman <ahuaman3@gatech.edu>
2014-04-18 17:31:41 -0400

    Changed script vision.sh to handle 4 cameras

--- alvarCode/visionClient.sh
+++ alvarCode/visionClient.sh
@@ -13,30 +13,29 @@ echo "-- Cleaned channels! --"
 # Create channels for 2 cameras
 ach -C cam0_channel -o 666  -m 10 -n 64
 ach -C cam1_channel -o 666  -m 10 -n 64
+ach -C cam2_channel -o 666  -m 10 -n 64
+ach -C cam3_channel -o 666  -m 10 -n 64
 ach -C krang_vision -o 666  -m 10 -n 64
 ach -C debug_channel -o 666  -m 10 -n 64
 
 # Fire camera processes. Assuming that:
 # Camera 0 is on /dev/video0
 # Camera 1 is on /dev/video1
+# Camera 2 is on /dev/video2
+# Camera 3 is on /dev/video3
 
-# For some reason we have to start camera 1 first, so keep the order
-echo "************************************"
-echo "[START] Firing up camera 1 [START]"
-echo "************************************"
-"${BIN_DIR}/camProcess" 1 1 &
-sleep 3s
+# We have to fire cameras in this order: 3-2-1-0 (order of /dev)
+for i in 3 2 1 0
+do
+    echo "************************************"
+    echo "[START] Firing up camera 3 [START]"
+    echo "************************************"
+    "${BIN_DIR}/camProcess" $i $i &
+    sleep 3s
 
+    echo "\n[DONE] Finishing starting camera $i"
 
-echo "\n[DONE] Finishing starting camera 1"
-
-echo "************************************"
-echo "[START] Firing up camera 0"
-echo "************************************"
-"${BIN_DIR}/camProcess" 0 0 &
-sleep 3s
-echo "[DONE] Finishing starting camera 0"
-
+done
 
 # Sleep for a bit to give the cameras time to run a bit
 echo "************************************"
